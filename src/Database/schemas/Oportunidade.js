d6813e705b81842716157e8c54979913c29708e3
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-18 17:54:38 -0400

    Criando estrutura inicial dos schemas

---
+++ src/Database/schemas/Oportunidade.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const OportunidadeSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        require: true,
+      },
+      creator_user_id: {
+        type: Number,
+        require: true,
+      },
+      user_id: Number,
+      person_id: Number,
+      org_id: Number,
+      stage_id: Number,
+      title: String,
+      value: Number,
+      currency: String,
+      add_time: Date,
+      active: Boolean,
+      deleted: Boolean,
+      status: String,
+      pipeline_id: Number,
+      products_count: Number,
+});
+
+module.exports = {
+    OportunidadeModel:  mongoose.model('Teste', OportunidadeSchema),
+    OportunidadeSchema
+};
---
+++ src/Database/schemas/Oportunidades.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const {OportunidadeSchema} = require('./Oportunidade');
+
+const TesteSchema = new mongoose.Schema({
+    data: {
+        type: Date,
+        require: true,
+    },
+    total: {
+        type: Number,
+        default: 0
+    },
+    contente: [OportunidadeSchema]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Teste', TesteSchema);
