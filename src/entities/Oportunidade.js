8e70416d6e9c5150536333b7cc14db04b44f395c
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-18 23:26:25 -0400

    alterando a entidade de oportunidades para atender somente aos campos necessários

--- src/entities/Oportunidade.js
+++ src/entities/Oportunidade.js
@@ -1,43 +1,29 @@
 class Oportunidade {
   constructor({
-    id,
-    creator_user_id,
-    user_id,
-    person_id,
-    org_id,
-    stage_id,
-    title,
-    value,
-    currency,
-    add_time,
-    active,
-    deleted,
-    status,
-    pipeline_id,
-    products_count,
+    id = null,
+    stage_id = null,
+    title = null,
+    value = null,
+    currency = null,
+    add_time = null,
+    active = null,
+    status = null,
   }) {
     this.id = id;
-    this.creator_user_id = creator_user_id;
-    this.user_id = user_id;
-    this.person_id = person_id;
-    this.org_id = org_id;
     this.stage_id = stage_id;
     this.title = title;
     this.value = value;
     this.currency = currency;
     this.add_time = add_time;
     this.active = active;
-    this.deleted = deleted;
     this.status = status;
-    this.pipeline_id = pipeline_id;
-    this.products_count = products_count;
   }
 
   entidadeValida() {
     const todasAsPropriedades = Object.getOwnPropertyNames(this);
     const propriedadesInvalidas = todasAsPropriedades
-        .filter(prop => !this[prop])
-        .map(prop => `${prop} inválida`);
+        .filter(prop => this[prop] === "undefined" || this[prop] === "null")
+        .map(prop => `campo [${prop}] inválido(a)`);
     
     return {
         valid: propriedadesInvalidas.length === 0,
@@ -45,6 +31,7 @@ class Oportunidade {
     };
 
   }
+
 }
 
 module.exports = Oportunidade;
