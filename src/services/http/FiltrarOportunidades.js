9de891863136a3859dedef904340798238c0f118
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-19 16:40:09 -0400

    corrigindo implementação da consulta http das oportunidades no pipedrive - Formatando resultado

--- src/services/http/FiltrarOportunidades.js
+++ src/services/http/FiltrarOportunidades.js
@@ -25,7 +25,52 @@ class FiltrarOportunidades {
                 });
                 
                 response.on('close', () => {
-                    resolv(data.join(''))
+                    const body = JSON.parse(data.join(''));
+
+                    if(!body.data){
+                        resolv(null);
+                    } else {
+                        
+                        const bodyFormatado = Array.from(body.data).map(el => {
+                            const {
+                                id,
+                                creator_user_id,
+                                user_id,
+                                person_id,
+                                org_id,
+                                stage_id,
+                                title,
+                                value,
+                                currency,
+                                add_time,
+                                active,
+                                deleted,
+                                status,
+                                pipeline_id,
+                                products_count
+                            } = el;
+
+                            return {
+                                id,
+                                creator_user_id: creator_user_id.id,
+                                user_id: user_id.id,
+                                person_id: person_id.owner_id,
+                                org_id: org_id.owner_id,
+                                stage_id,
+                                title,
+                                value,
+                                currency,
+                                add_time,
+                                active,
+                                deleted,
+                                status,
+                                pipeline_id,
+                                products_count
+                            }
+                        })
+    
+                        resolv(bodyFormatado)
+                    }
                 })
 
                 response.on('error', (err) => {
