1e0c2fb097bdc13d8740af930987ac445818235a
Delfio Francisco <delfio_eu@hotmail.com>
2021-06-18 22:08:52 -0400

    criando função para registrar os logs da aplicação

---
+++ src/utils/Logger.js
@@ -0,0 +1,47 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+class Logger {
+  #pathPadraoLogger;
+  constructor() {
+    this.#pathPadraoLogger = path.resolve(__dirname, "..", "..", "temp");
+    console.log("path padrão -", this.#pathPadraoLogger);
+  }
+
+  #formatarData() {
+    return new Date()
+      .toLocaleDateString("pt-br", {
+        dateStyle: "short",
+        timeStyle: "medium",
+      })
+      .replace(/\//g, "-")
+      .replace(/:/g, " ");
+  }
+
+  async gravarLog(log) {
+    const cabecalho = this.#formatarData();
+
+    const pathFormatado = path.resolve(
+      this.#pathPadraoLogger,
+      `log-${cabecalho.trim()}.log`
+    );
+
+    const novoLog = `[${cabecalho}] : ${log}\n`;
+
+    return fs
+      .access(pathFormatado)
+      .then(() => {
+        return fs.appendFile(pathFormatado, novoLog, {
+          encoding: "utf-8",
+        });
+      })
+      .catch(() => {
+        return fs.writeFile(pathFormatado, novoLog, {
+          encoding: "utf-8",
+          flag: "wx",
+        });
+      });
+  }
+}
+
+module.exports = Logger;
